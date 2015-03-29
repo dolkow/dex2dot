dd224a9d844e56cd419464170d6feba32e9d591e
Snild Dolkow <snild@dolkow.se>
2015-03-29 23:01:40 +0200

    Fixed basic blocks
    Doing it right this time.
    
    Better late than never, and better correct than clever. :)
    
    (I really hope it's correct now. It'll be easier to tell once more
    branch types are added and blocks start showing their code.)
    
    This commit's soundtrack: Ben Prunty - MilkyWay (Battle)

--- dex/basicblock.py
+++ dex/basicblock.py
@@ -12,108 +12,29 @@ class BasicBlock(object):
 		self.ops   = [] # list of instruction operations, in address order
 		self.args  = [] # list of instruction everything-else, in address order
 		self.catches = None
-		self.next = None
-		self.cond_next = None
-		self.switch_next = None
+		self.succ = None
 
-	def add_instruction(self, addr, op, args):
-		assert not self.addrs or self.addrs[-1] < addr
-		self.addrs.append(addr)
-		self.ops.append(op)
-		self.args.append(args)
-
-	def split(self, addr):
-		''' split this block, returning a new block *ending* at addr '''
-		low = BasicBlock(self.name)
-		self.name = 'block_%04x' % addr
-		low.catches = self.catches # identical catches.
-
-		# we're keeping our own outs, and the new block will have us as next.
-		low.next = self
-
-		# now split the code
-		assert self.addrs
-		ix = self.addrs.index(addr)
-		assert ix > 0
-		low.addrs, self.addrs = self.addrs[:ix], self.addrs[ix:]
-		low.ops,   self.ops   = self.ops[:ix],   self.ops[ix:]
-		low.args,  self.args  = self.args[:ix],  self.args[ix:]
-
-		return low
-
-	def finished(self):
-		return self.next is not None
-
-class BlockList(object):
-	def __init__(self):
-		entry  = BasicBlock('__func_entry')
-		exit   = BasicBlock('__func_exit')
-		zero   = BasicBlock(0)
-		entry.next = zero
-		self.addrs  = [-1, 0, 2**16] # ordered by address
-		self.blocks = [entry, zero, exit] # same length and order as addrs
-
-	def find_index(self, addr):
-		''' find the index of the block that contains addr '''
-		assert addr >= 0 and addr < 2**16 # dalvik method length is 16-bit
-		prev = 0
-		for ix, blockstart in enumerate(self.addrs):
-			if addr < blockstart:
-				assert ix != 0
-				return ix-1
-		assert False, 'BUG: should have found the index!' # due to exit block
-
-	def get_or_create(self, addr):
-		''' get basic block that contains addr, creating it if needed '''
-		ix = self.find_index(addr)
-		block = self.blocks[ix]
-		if block.finished():
-			block = BasicBlock(addr)
-			self.addrs.insert(ix+1, addr)
-			self.blocks.insert(ix+1, block)
-		return block
-
-	def get_or_split(self, addr):
-		''' get basic block starting at addr, splitting as needed '''
-		ix = self.find_index(addr)
-		block = self.blocks[ix]
-		if self.addrs[ix] == addr:
-			return block
-		lower = block.split(addr)
-		self.addrs.insert(ix+1, addr) # yes, the ix diff here is intentional:
-		self.blocks.insert(ix, lower) # lower inherits block's old addr
-		return block
-
-	def add_instruction(self, addr, op, args):
-		block = self.get_or_create(addr)
-		block.add_instruction(addr, op, args)
-		if op.startswith('goto'):
-			target = int(args.split()[0], 16)
-			target = self.get_or_split(target)
-			block.next = target
-		elif op.startswith('if-'):
-			pass # TODO
-		elif op == 'packed-switch':
-			pass # TODO
-		elif op == 'sparse-switch':
-			pass # TODO
-		elif op == 'throw':
-			pass # TODO
-		elif op.startswith('return'):
-			pass # TODO
-
-def makeblocks(dexfile, code, catches):
+def codeparser(code):
 	codere = re.compile(r"^[0-9a-f]+:(?: [0-9a-f]{4})+\s+(?:\.\.\. )?\s*\|"
 	                  + r"([0-9a-f]{4}): (\S+) (\S.*)$")
 	stringre = re.compile(r'" // string@[0-9a-f]{4}$')
-	blocklist = BlockList()
-
 	generator = (line for line in code)
+	found_switch_data = False
+	last_addr = -1
 	for line in generator:
 		m = codere.match(line)
 		assert m, 'BUG: code regex does not match line "%s"' % line
 		addr, op, args = m.groups()
 		addr = int(addr, 16)
+
+		assert addr > last_addr
+		last_addr = addr
+
+		if op in ('packed-switch-data', 'sparse-switch-data'):
+			found_switch_data = True
+			continue # don't include in any basic blocks
+
+		assert not found_switch_data, 'I assumed switch data was always last'
 
 		if op.startswith('const-string'):
 			# join multi-line const strings
@@ -123,9 +44,73 @@ def makeblocks(dexfile, code, catches):
 				args += '\\n'
 				args += line
 
-		if op in ('packed-switch-data', 'sparse-switch-data'):
-			continue # don't include in any basic blocks
+		yield addr, op, args
 
-		blocklist.add_instruction(addr, op, args)
+def makeblocks(dexfile, code, catches):
+	blockstarts = set() # addresses of basic blocks' first instruction
+	jumps = {} # src addr -> {cond -> dst addr};
+	# cond True  = branch condition OK
+	# cond <int> = switch value
 
-	return blocklist.blocks
+	def addjmp(cond, src, dst):
+		blockstarts.add(dst)
+		if src not in jumps:
+			jumps[src] = {}
+		jumps[src][cond] = dst
+
+	last_branched = True
+
+	# find all branches
+	for addr, op, arg in codeparser(code):
+		import sys
+		print('code: %04x:' % addr, op, arg, file=sys.stderr)
+
+		if last_branched:
+			blockstarts.add(addr)
+
+		last_branched = True
+		if op.startswith('goto'):
+			addjmp(None, addr, int(arg.split()[0], 16))
+#		elif op.startswith('if-'):
+#			pass # TODO
+#		elif op == 'packed-switch':
+#			pass # TODO
+#		elif op == 'sparse-switch':
+#			pass # TODO
+#		elif op == 'throw':
+#			pass # TODO
+#		elif op.startswith('return'):
+#			pass # TODO
+		else:
+			last_branched = False
+	assert last_branched, 'no branch at function end'
+
+	# create basic blocks
+	block = BasicBlock('func_entry')
+	fexit = BasicBlock('func_exit')
+	fexit.succ = {}
+	blocks = {-1:block, -2:fexit} # start addr -> block
+	for addr, op, arg in codeparser(code):
+		if addr in blockstarts:
+			prev = block
+			block = BasicBlock(addr)
+			blocks[addr] = block
+			if prev.succ is None:
+				prev.succ = {}
+			if None not in prev.succ:
+				prev.succ[None] = addr # link fallthrough
+		assert block.succ is None
+		block.addrs.append(addr)
+		block.ops.append(op)
+		block.args.append(arg)
+		if addr in jumps:
+			block.succ = jumps[addr]
+	assert block.succ is not None, 'function has no branch at the end'
+
+	# convert branch targets from address to block reference
+	for block in blocks.values():
+		for cond in block.succ:
+			dst_addr = block.succ[cond]
+			block.succ[cond] = blocks[dst_addr]
+
+	return tuple(blocks.values())
